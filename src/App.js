b011144ebc618683ae2024faf769a54e662131e2
jondrews <mistersquiggle@hotmail.com>
2022-07-25 11:22:03 +1000

    Add bootstrap. Test buttons & stack.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css'
+import { Stack, Button } from 'react-bootstrap';
 
 const App = () => {
   return (
     <div className="App">
       <p>Tunes app!</p>
+      <Stack direction="horizontal" gap={2}>
+        <Button as="a" variant="primary">
+          Primary button test
+        </Button>
+        <Button as="a" variant="outline-success">
+          Success button test
+        </Button>
+      </Stack>
     </div>
   )
 }
